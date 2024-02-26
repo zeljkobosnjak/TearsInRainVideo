c64d4e7369583e290a26582f01eb9a953ca56719
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:52:59 +0100

    Update RentalRepository.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/repository/RentalRepository.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/repository/RentalRepository.java
@@ -1,2 +1,8 @@
-package org.tearsinrainvideo.tearsinrainvideo.repository;public interface RentalRepository {
+package org.tearsinrainvideo.tearsinrainvideo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tearsinrainvideo.tearsinrainvideo.model.Rental;
+
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+
 }
