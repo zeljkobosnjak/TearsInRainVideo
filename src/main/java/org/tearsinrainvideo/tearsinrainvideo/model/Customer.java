a21d7e0a5aab61f67c6caa2faf03ffa07c6e0500
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:51:00 +0100

    Update Customer.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/model/Customer.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/model/Customer.java
@@ -1,2 +1,25 @@
-package org.tearsinrainvideo.tearsinrainvideo.model;public class Customer {
+package org.tearsinrainvideo.tearsinrainvideo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
 }
