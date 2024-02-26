493c33934d21dfc10026aa56e159da5465f316b4
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:54:54 +0100

    Update RentalService.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/service/RentalService.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/service/RentalService.java
@@ -1,2 +1,8 @@
-package org.tearsinrainvideo.tearsinrainvideo.service;public interface RentalService {
+package org.tearsinrainvideo.tearsinrainvideo.service;
+
+import org.tearsinrainvideo.tearsinrainvideo.model.Rental;
+
+public interface RentalService {
+    Rental saveRental(Rental rental);
+    Rental processReturn(Long rentalId, Rental returnDetails);
 }
