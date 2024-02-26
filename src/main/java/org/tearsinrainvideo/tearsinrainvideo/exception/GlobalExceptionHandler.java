1075500fcd934327459638d2fac2861ccaa7b2f3
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:49:54 +0100

    Update GlobalExceptionHandler.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/exception/GlobalExceptionHandler.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/exception/GlobalExceptionHandler.java
@@ -1,2 +1,46 @@
-package org.tearsinrainvideo.tearsinrainvideo.exception;public class GlobalExceptionHandler {
+package org.tearsinrainvideo.tearsinrainvideo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FilmNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleFilmNotFoundException(FilmNotFoundException ex, WebRequest request) {
+        ApiErrorResponse response = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RentalProcessException.class)
+    public ResponseEntity<ApiErrorResponse> handleRentalProcessException(RentalProcessException ex, WebRequest request) {
+        ApiErrorResponse response = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleAllUncaughtException(Exception ex, WebRequest request) {
+        ApiErrorResponse response = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
