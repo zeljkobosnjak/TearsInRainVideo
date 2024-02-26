d5570b5ad13b5724f24412336f2ebe2a8b59cecb
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:51:45 +0100

    Update Rental.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/model/Rental.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/model/Rental.java
@@ -1,2 +1,38 @@
-package org.tearsinrainvideo.tearsinrainvideo.model;public class Rental {
+package org.tearsinrainvideo.tearsinrainvideo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    private LocalDate rentalDate;
+    private int rentalDays;
+    private LocalDate returnDate;
+    private boolean returned;
+    private double lateFee;
 }
