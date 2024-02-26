c05949fb32d5d59c1c93b675ad680eb6ae88fd2b
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:54:05 +0100

    Update FilmService.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/service/FilmService.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/service/FilmService.java
@@ -1,2 +1,12 @@
-package org.tearsinrainvideo.tearsinrainvideo.service;public interface FilmService {
+package org.tearsinrainvideo.tearsinrainvideo.service;
+
+import org.tearsinrainvideo.tearsinrainvideo.model.Film;
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmService {
+    Film saveFilm(Film film);
+    Optional<Film> findById(Long id);
+    List<Film> findAll();
+    void deleteFilm(Long id);
 }
