6ea0ee7dfcc4be9e31baa6a4cfc59c8f16dc527e
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:47:36 +0100

    Update FilmController.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/controller/FilmController.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/controller/FilmController.java
@@ -1,2 +1,44 @@
-package org.tearsinrainvideo.tearsinrainvideo.controller;public class FilmController {
+package org.tearsinrainvideo.tearsinrainvideo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.tearsinrainvideo.tearsinrainvideo.model.Film;
+import org.tearsinrainvideo.tearsinrainvideo.service.FilmService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+
+    private final FilmService filmService;
+
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Film> addFilm(@RequestBody Film film) {
+        return ResponseEntity.ok(filmService.saveFilm(film));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Film> getFilm(@PathVariable Long id) {
+        Film film = filmService.findById(id)
+                .orElseThrow(() -> new RuntimeException("Film not found"));
+        return ResponseEntity.ok(film);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Film>> getAllFilms() {
+        return ResponseEntity.ok(filmService.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFilm(@PathVariable Long id) {
+        filmService.deleteFilm(id);
+        return ResponseEntity.noContent().build();
+    }
 }
