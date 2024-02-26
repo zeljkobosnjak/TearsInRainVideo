7cb4f34c211496672a6a20eb6a33be2fa2949f20
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:51:24 +0100

    Update Film.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/model/Film.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/model/Film.java
@@ -1,2 +1,29 @@
-package org.tearsinrainvideo.tearsinrainvideo.model;public class Film {
+package org.tearsinrainvideo.tearsinrainvideo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.tearsinrainvideo.tearsinrainvideo.enums.FilmType;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private FilmType type;
 }
