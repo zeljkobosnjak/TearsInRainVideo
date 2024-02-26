58b499abb4a59d83e2b95ba2d51483c2e9d6feb1
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:53:23 +0100

    Update CustomerService.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/service/CustomerService.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/service/CustomerService.java
@@ -1,2 +1,12 @@
-package org.tearsinrainvideo.tearsinrainvideo.service;public interface CustomerService {
+package org.tearsinrainvideo.tearsinrainvideo.service;
+
+import org.tearsinrainvideo.tearsinrainvideo.model.Customer;
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+    Customer saveCustomer(Customer customer);
+    Optional<Customer> findCustomerById(Long id);
+    List<Customer> findAllCustomers();
+    void deleteCustomer(Long id);
 }
