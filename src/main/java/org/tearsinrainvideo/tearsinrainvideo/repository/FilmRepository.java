f8303bbfd7673d48c130445b9e967a1a695758a8
zeljkobosnjak <35273545+zeljkobosnjak@users.noreply.github.com>
2024-02-26 17:52:39 +0100

    Update FilmRepository.java

--- src/main/java/org/tearsinrainvideo/tearsinrainvideo/repository/FilmRepository.java
+++ src/main/java/org/tearsinrainvideo/tearsinrainvideo/repository/FilmRepository.java
@@ -1,2 +1,8 @@
-package org.tearsinrainvideo.tearsinrainvideo.repository;public interface FilmRepository {
+package org.tearsinrainvideo.tearsinrainvideo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tearsinrainvideo.tearsinrainvideo.model.Film;
+
+public interface FilmRepository extends JpaRepository<Film, Long> {
+
 }
